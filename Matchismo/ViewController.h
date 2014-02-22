e05daf5dfa189c97e73fdc87dab8f6e56ac85a78
Art M <art.mostofi@gmail.com>
2014-02-21 17:46:49 -0800

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Art Mostofi on 2/21/14.
+//  Copyright (c) 2014 Art Mostofi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Art Mostofi on 2/21/14.
+//  Copyright (c) 2014 Art Mostofi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
